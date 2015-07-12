69a52acde3843beaeaaa63de8c245d0a56eace4a
Theo Franzén <franzen.theo@gmail.com>
2015-07-12 16:17:35 +0200

    Learning Open GL - Shapes and projection

---
+++ TestingOpenGL/app/src/main/java/se/hanell/test/testingopengl/CustomGLSurfaceView.java
@@ -0,0 +1,18 @@
+package se.hanell.test.testingopengl;
+
+import android.content.Context;
+import android.opengl.GLSurfaceView;
+
+public class CustomGLSurfaceView extends GLSurfaceView {
+
+    private final CustomGLRenderer customGLRenderer;
+
+    public CustomGLSurfaceView(Context context) {
+        super(context);
+
+        setEGLContextClientVersion(2);
+        customGLRenderer = new CustomGLRenderer();
+        setRenderer(customGLRenderer);
+        setRenderMode(GLSurfaceView.RENDERMODE_WHEN_DIRTY);
+    }
+}
